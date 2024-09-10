0cadef2d8013976f3b2cf0d6c6ca8a36c455a2ee
bboubacar <bboubacarfr@gmail.com>
2024-09-10 13:52:09 +0200

    Mise à niveau de vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true  // permet à Vite de servir sur toutes les interfaces (0.0.0.0)
+    host: true,  // permet à Vite de servir sur toutes les interfaces (0.0.0.0)
+    port: 5173,
   }
 })
